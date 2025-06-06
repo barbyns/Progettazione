ad8192979968e7d04d0424c48bae292c58a56330
Barbara <barbyns95@gmail.com>
2025-06-06 20:57:59 +0200

    add

---
+++ src/main/java/it/epicode/Progettazione/ProgettazioneApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.Progettazione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettazioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettazioneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/Progettazione/ProgettazioneApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.Progettazione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettazioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
